9a95d729fd6017016e238dc880667cc2fc789013
Jonathan Lyons <jrl08d@my.fsu.edu>
2015-11-18 16:53:44 -0500

    create vanilla app for testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JrubyTest::Application.config.secret_token = '969ab52696f00e67843b7c99daf04c730d9e43f2f2722248b456ae396dcaf841514e966db281c981cda1769e4e43a2b9eac4c3681aadf4885a491f4970c2650a'
